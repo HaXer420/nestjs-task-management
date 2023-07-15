7cc3e57950d3b5bcf9668c988821f46d002022f4
Massab Iftikhar <massabiftkhar@gmail.com>
2023-07-15 15:08:32 +0500

    env also added

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 import * as config from 'config';
+
+dotenv.config({ path: './config/.env' });
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get<any>('server');
-  const port = serverConfig.port;
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
-  logger.log(`App is Listening on Port: ${port}`);
+  logger.log(`App is Listening on Port : ${port}`);
 }
 bootstrap();
